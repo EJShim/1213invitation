9cf324f2337bed3907e384096aa2eef6a86b15df
EJShim <soulrommel@gmail.com>
2019-11-06 14:32:09 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  componentDidMount(){
+  }
+
+
+  render(){
+    return (
+      <div className="bgimg w3-display-container w3-animate-opacity w3-text-white">
+        <div className="w3-display-topleft w3-padding-large w3-xlarge">
+          Logo
+        </div>
+
+        <div className="w3-display-middle">
+          <h1 className="w3-jumbo w3-animate-top">COMING SOON</h1>
+          <hr className="w3-border-grey" style={{margin:"auto",width:40+'%'}}></hr>
+          <p class="w3-large w3-center"> 35 days left</p>
+        </div>
+
+        <div className="w3-display-bottomleft w3-padding-large">
+          Powered by <a href="https://ejshim.github.io">EJShim</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
