bd4382f33dd3fe2195204007e2322a559e492b68
EJShim <soulrommel@gmail.com>
2019-11-06 14:41:16 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import background from './resource/img/forestbridge.jpg';
 import './App.css';
 
 class App extends Component {
@@ -9,8 +10,11 @@ class App extends Component {
 
 
   render(){
+
+
+
     return (
-      <div className="bgimg w3-display-container w3-animate-opacity w3-text-white">
+      <div className="bgimg w3-display-container w3-animate-opacity w3-text-white" style={{backgroundImage:'url('+ background + ')'}}>
         <div className="w3-display-topleft w3-padding-large w3-xlarge">
           Logo
         </div>
@@ -18,7 +22,7 @@ class App extends Component {
         <div className="w3-display-middle">
           <h1 className="w3-jumbo w3-animate-top">COMING SOON</h1>
           <hr className="w3-border-grey" style={{margin:"auto",width:40+'%'}}></hr>
-          <p class="w3-large w3-center"> 35 days left</p>
+          <p className="w3-large w3-center"> 35 days left</p>
         </div>
 
         <div className="w3-display-bottomleft w3-padding-large">
