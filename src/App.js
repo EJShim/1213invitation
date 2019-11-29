f3b074dfb99a5e118a8daada73cb399a6a8a8c29
EJShim <soulrommel@gmail.com>
2019-11-29 18:19:50 +0900

    update

--- src/App.js
+++ src/App.js
@@ -62,7 +62,14 @@ class App extends Component {
         <section className="map-container">
           
           <div className="map-text">
-              <p>장소 안내 영상</p>
+            <ul>
+              <p>Date : 2019.12.13</p>
+              <li>Time : 7:00 p.m - 5:00 a.m.</li>
+              <li>Where : SOUL SOUP</li>
+              <li>Dress Code : Movie Star</li>
+              <li>Entrance fee : 35,000 won</li>
+              <li>*각자 마실 음료 가져오기</li>
+            </ul>
           </div>
           <div className="map-content">
             <iframe title="location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3695.1957656052987!2d127.05614041254037!3d37.58824303692843!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb6c7c8d3245%3A0x6d91e4dd74c9688d!2z7IaM7Jq47Iiy!5e0!3m2!1sko!2sgm!4v1573040435588!5m2!1sko!2sgm" width="100%" height="100%" frameBorder="0" style={{border:0}} allowFullScreen=""/>
