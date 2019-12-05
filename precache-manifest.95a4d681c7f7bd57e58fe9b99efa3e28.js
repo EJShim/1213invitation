fb7748daf61879e24d1faa4cfe4249a84fa41c1b
EJShim <soulrommel@gmail.com>
2019-12-05 14:40:08 +0900

    Updates

---
+++ precache-manifest.95a4d681c7f7bd57e58fe9b99efa3e28.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c73d61b473b8b118dfbd68202f10095d",
+    "url": "/1213invitation/index.html"
+  },
+  {
+    "revision": "05f88395db74dd289a6d",
+    "url": "/1213invitation/static/css/main.505cefe8.chunk.css"
+  },
+  {
+    "revision": "2e4c29ceeeec76d9129b",
+    "url": "/1213invitation/static/js/2.d5f08eab.chunk.js"
+  },
+  {
+    "revision": "05f88395db74dd289a6d",
+    "url": "/1213invitation/static/js/main.11355760.chunk.js"
+  },
+  {
+    "revision": "8ef1233626fc665da699",
+    "url": "/1213invitation/static/js/runtime-main.109d73d8.js"
+  },
+  {
+    "revision": "d791462d9c2dd0f3e0805805e005ef91",
+    "url": "/1213invitation/static/media/10.d791462d.jpg"
+  },
+  {
+    "revision": "58f2526db8a0cd54935e735bea9b25f5",
+    "url": "/1213invitation/static/media/11.58f2526d.jpg"
+  },
+  {
+    "revision": "689db91c81a934709cb64d8a8417ba21",
+    "url": "/1213invitation/static/media/12.689db91c.jpg"
+  },
+  {
+    "revision": "a545e1f8c9073af6e163ef6ba5acfc04",
+    "url": "/1213invitation/static/media/13.a545e1f8.jpg"
+  },
+  {
+    "revision": "2f161285b52fdbb14e9c68c47baf4e83",
+    "url": "/1213invitation/static/media/14.2f161285.jpg"
+  },
+  {
+    "revision": "d8ba866ab87bcb07cd2cfe8c29f4ca73",
+    "url": "/1213invitation/static/media/15.d8ba866a.jpg"
+  },
+  {
+    "revision": "7037189dbc6934d9f0d7abd17b111865",
+    "url": "/1213invitation/static/media/16.7037189d.jpg"
+  },
+  {
+    "revision": "9df4eec90b266dd9cc752463c31b79a5",
+    "url": "/1213invitation/static/media/17.9df4eec9.jpg"
+  },
+  {
+    "revision": "0480669db008ee13ca36c62554bdeb57",
+    "url": "/1213invitation/static/media/18.0480669d.jpg"
+  },
+  {
+    "revision": "96b58b25658d7cbe495221f9ca9688ae",
+    "url": "/1213invitation/static/media/2.96b58b25.png"
+  },
+  {
+    "revision": "d7023f94df418cc5b82973e60669eeac",
+    "url": "/1213invitation/static/media/5.d7023f94.jpg"
+  },
+  {
+    "revision": "fe31c70470fa8b63eaf69633f4232e98",
+    "url": "/1213invitation/static/media/6.fe31c704.jpg"
+  },
+  {
+    "revision": "6774adb2528832d5b03c4db0b4caa529",
+    "url": "/1213invitation/static/media/7.6774adb2.jpg"
+  },
+  {
+    "revision": "6f270964e2acdec115647a2c174ff167",
+    "url": "/1213invitation/static/media/8.6f270964.jpg"
+  },
+  {
+    "revision": "982bed91f8710737480d2b02904bb830",
+    "url": "/1213invitation/static/media/9.982bed91.jpg"
+  },
+  {
+    "revision": "d861e2bc668bec1931440c284cac5271",
+    "url": "/1213invitation/static/media/kistlogo.d861e2bc.ico"
+  },
+  {
+    "revision": "af439b554def1c02386a90fbfdb5eafe",
+    "url": "/1213invitation/static/media/kivengerse.af439b55.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/1213invitation/precache-manifest.fd05edf0ee140d27bb228cd318d34e74.js"
+  "/1213invitation/precache-manifest.95a4d681c7f7bd57e58fe9b99efa3e28.js"
 );
 
 self.addEventListener('message', (event) => {
