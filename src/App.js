17d22770352b677e0c4c0fc7cb28976bce725547
EJShim <soulrommel@gmail.com>
2019-12-06 15:49:33 +0900

    update

--- src/App.js
+++ src/App.js
@@ -27,9 +27,123 @@ class App extends Component {
   }
 
   componentDidMount(){        
-    
+    this.carousel();
+    //this.snow();
   }
 
+  // snow(){
+      
+  //   var COUNT = 300;
+  //   var masthead = document.querySelector('.map-text');
+  //   var canvas = document.createElement('canvas');
+  //   var ctx = canvas.getContext('2d');
+  //   var width = masthead.clientWidth;
+  //   var height = masthead.clientHeight;
+  //   var i = 0;
+  //   var active = false;
+
+  //   function onResize() {
+
+  //     width = masthead.clientWidth;
+  //     height = masthead.clientHeight;
+  //     canvas.width = width;
+  //     canvas.height = height;
+  //     ctx.fillStyle = '#FFF';
+
+  //     var wasActive = active;
+  //     active = width > 600;
+
+  //     if (!wasActive && active)
+  //       window.requestAnimFrame(update);
+  //   }
+
+  //   var Snowflake = function () {
+  //     this.x = 0;
+  //     this.y = 0;
+  //     this.vy = 0;
+  //     this.vx = 0;
+  //     this.r = 0;
+
+  //     this.reset();
+  //   }
+
+  //   Snowflake.prototype.reset = function() {
+  //     this.x = Math.random() * width;
+  //     this.y = Math.random() * -height;
+  //     this.vy = 1 + Math.random() * 3;
+  //     this.vx = 0.5 - Math.random();
+  //     this.r = 1 + Math.random() * 2;
+  //     this.o = 0.5 + Math.random() * 0.5;
+  //   }
+
+  //   canvas.style.position = 'absolute';
+  //   canvas.style.left = 0;
+
+  //   var snowflakes = [], snowflake;
+  //   for (i = 0; i < COUNT; i++) {
+  //     snowflake = new Snowflake();
+  //     snowflake.reset();
+  //     snowflakes.push(snowflake);
+  //   }
+
+  //   function update() {
+  //     ctx.clearRect(0, 0, width, height);
+
+  //     if (!active)
+  //       return;
+
+  //     for (i = 0; i < COUNT; i++) {
+  //       snowflake = snowflakes[i];
+  //       snowflake.y += snowflake.vy;
+  //       snowflake.x += snowflake.vx;
+
+  //       ctx.globalAlpha = snowflake.o;
+  //       ctx.beginPath();
+  //       ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
+  //       ctx.closePath();
+  //       ctx.fill();
+
+  //       if (snowflake.y > height) {
+  //         snowflake.reset();
+  //       }
+  //     }
+
+  //     window.requestAnimFrame(update);
+  //   }
+
+  //   // shim layer with setTimeout fallback
+  //   window.requestAnimFrame = (function(){
+  //     return  window.requestAnimationFrame       || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame    ||
+  //             function( callback ){
+  //               window.setTimeout(callback, 1000 / 60);
+  //             };
+  //   })();
+
+  //   onResize();
+  //   window.addEventListener('resize', onResize, false);
+
+  //   masthead.appendChild(canvas);
+  // }
+
+  carousel() {
+    let i;
+    const x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    this.myIndex++;    
+    if (this.myIndex > x.length) {this.myIndex = 1}    
+    x[this.myIndex-1].style.display = "block";  
+    setTimeout(()=>{this.carousel()}, 9000);    
+  }
+
+  shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
 
   
 
@@ -37,9 +151,10 @@ class App extends Component {
   render(){
 
     const slides = [slide2, slide5,slide6,slide7,slide8,slide9,slide10,slide11,slide12,slide13,slide14,slide15,slide16,slide17,slide18];
+    this.shuffle(slides);
 
     const slideElement = slides.map((image, idx)=>{
-      return (<img key={idx} src={image}/>)
+      return ( <img key={idx} className="mySlides w3-animate-fading" src={image} />)
     });
     return (
       <div ref="container" className="container">
@@ -54,6 +169,7 @@ class App extends Component {
             <p className="shadow"> 장소 : 회기역 소울숲</p>
             <h1 className="shadow"> 테마 : Movie Star - 베스트 드레서 시상</h1>
             <p className="shadow"> 게임 및 경품 추첨, 소정의 상품 지급</p>
+            <p className="shadow"> 각자 마실 거 가져오기</p>
             <p className="shadow"> 참가비 : 35,000 원</p>
             <p className="shadow"> 우리은행 1002-048-671946 (박승빈)</p>
           </div>
@@ -62,9 +178,8 @@ class App extends Component {
           </div>          
         </section>
 
-        <section className="starwars">
-          <div className="fade"/>
-          <div className="crawl">
+        <section className="carousel">
+          <div className="w3-content w3-section" style={{maxWidth:"100%"}}>
             {slideElement}
           </div>
         </section>
--- src/container/Main.js
+++ src/container/Main.js
@@ -12,24 +12,17 @@ class Main extends Component{
         const then = new Date("december 13, 2019");
         const gap = now.getTime() - then.getTime();
         const day = Math.abs( Math.floor(gap / (1000 * 60 * 60 * 24)));
+
+        const backgroundStyle = {
+            backgroundImage : 'url('+ background + ')'            
+        };
+
         return(
-            <div className="bgimg w3-display-container w3-animate-opacity w3-text-white" 
-                style={{backgroundImage:'linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url('+ background + ')'}}>
+            <div className="bgimg" style={backgroundStyle}>                
                 <div className="w3-display-topleft w3-padding-large w3-xlarge">
                     <img className="logo" src={kistlogoIcon} alt="logo"></img>
                 </div>
-
-                {/* <div className="w3-display-middle">
-                    <h1 className="w3-jumbo w3-animate-top"> COMING SOON</h1>
-                    <hr className="w3-border-grey" style={{margin:"auto",width:40+'%'}}></hr>
-                    <p className="w3-medium w3-center"> Dec 13, 2019 </p>
-                    <p className="w3-large w3-center">{day} days left</p>
-                </div>  */}
-
-                {/* <div className="w3-display-bottomleft w3-padding-large">
-                    Powered by <a href="https://ejshim.github.io">EJShim</a>
-                </div> */}
-                </div>
+            </div>
         );
     }
 
